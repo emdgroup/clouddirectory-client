32cd178da5876dc854ad6c3787d8632f61145edf
Moritz Onken <moritz.onken@merckgroup.com>
2018-06-23 16:54:05 +0200

    don't delete schemas/directories for now

--- tests/inc/setup.js
+++ tests/inc/setup.js
@@ -5,16 +5,16 @@ const AWS = require('aws-sdk'),
 let rand = crypto.randomBytes(6).toString('hex');
 
 module.exports = async () => {
-  let devSchemas = await client.listDevelopmentSchemaArns({ MaxResults: 30 }).promise();
-  let pubSchemas = await client.listPublishedSchemaArns({ MaxResults: 30 }).promise();
-  await Promise.all(devSchemas.SchemaArns.map(s => client.deleteSchema({ SchemaArn: s }).promise()));
-  await Promise.all(pubSchemas.SchemaArns.map(s => client.deleteSchema({ SchemaArn: s }).promise()));
-  let directories = await client.listDirectories({ MaxResults: 30, state: 'ENABLED' }).promise();
-  await Promise.all(
-    directories.Directories.map(
-      d => client.disableDirectory({ DirectoryArn: d.DirectoryArn }).promise().then(() => client.deleteDirectory({ DirectoryArn: d.DirectoryArn }).promise())
-    )
-  );
+  // let devSchemas = await client.listDevelopmentSchemaArns({ MaxResults: 30 }).promise();
+  // let pubSchemas = await client.listPublishedSchemaArns({ MaxResults: 30 }).promise();
+  // await Promise.all(devSchemas.SchemaArns.map(s => client.deleteSchema({ SchemaArn: s }).promise()));
+  // await Promise.all(pubSchemas.SchemaArns.map(s => client.deleteSchema({ SchemaArn: s }).promise()));
+  // let directories = await client.listDirectories({ MaxResults: 30, state: 'ENABLED' }).promise();
+  // await Promise.all(
+  //   directories.Directories.map(
+  //     d => client.disableDirectory({ DirectoryArn: d.DirectoryArn }).promise().then(() => client.deleteDirectory({ DirectoryArn: d.DirectoryArn }).promise())
+  //   )
+  // );
   let devSchema = await client.createSchema({
     Name: 'test-cdclient-' + rand,
   }).promise().then(res => client.putSchemaFromJson({
